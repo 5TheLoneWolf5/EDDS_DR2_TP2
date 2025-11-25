4048b2b928fda22a6f1c0ead6c7e8e201d8d2a41
Felipe Alves <89516641+5TheLoneWolf5@users.noreply.github.com>
2025-11-25 15:54:21 -0300

    Update Exercício_03.java

--- src/main/java/org/example/Exercício_03.java
+++ src/main/java/org/example/Exercício_03.java
@@ -1,5 +1,11 @@
 package org.example;
 
+/*
+* Foi criado uma classe "ClienteNull" que extende o comportamento da classe "Cliente". Com ela, podemos deixar o código mais flexível e consertar problemas relacionado com clientes nulos que estão no pedido.
+* Pode-se sobrescrever os métodos da superclasse para executar algo ou nada na implementação da classe nula.
+* Abaixo segue a implementação.
+*/
+
 class App {
     public static void main(String[] args) {
         Cliente cliente = criarCliente();
