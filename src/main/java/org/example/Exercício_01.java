d05bdadab366a33d6f8e3826a7c9cafd197809e0
Felipe Alves <89516641+5TheLoneWolf5@users.noreply.github.com>
2025-11-25 15:42:14 -0300

    Update Exercício_01.java

--- src/main/java/org/example/Exercício_01.java
+++ src/main/java/org/example/Exercício_01.java
@@ -1,5 +1,10 @@
 package org.example;
 
+/*
+* Os nomes escolhidos fazem sentido semântico e facilitam na leitura do código. 
+* É utilizado verbos no nome da função para descrever o seu comportamento. Os parâmetros são nomeados de forma simples, mas eficaz. A variável "resultadoSoma" descreve a declaração de forma intuitiva.
+* */
+
 class A {
     public static int somarEMultiplicarPorDois(int valor1, int valor2) {
         int resultadoSoma = valor1 + valor2;
