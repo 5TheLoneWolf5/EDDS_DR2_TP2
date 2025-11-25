a0dc9450d8ffce34622120872bb974f40e2c337f
Felipe Alves <89516641+5TheLoneWolf5@users.noreply.github.com>
2025-11-25 15:47:40 -0300

    Update Exercício_02.java

--- src/main/java/org/example/Exercício_02.java
+++ src/main/java/org/example/Exercício_02.java
@@ -1,5 +1,13 @@
 package org.example;
 
+/*
+* A seguir é o código com as melhorias aplicadas. As correções realizadas foram:
+*
+* Constantes são declaradas no topo da classe e com nomes digitados em letra maiúscula, seguindo convenções do código limpo;
+* A regra de negócio para descontos foi corrigida;
+* Melhores nomes foram escolhidos e o código está mais legível.
+* */
+
 class Desconto {
     private final double VALOR_PARA_DESCONTO = 1000;
     private final double DESCONTO = 0.9;
