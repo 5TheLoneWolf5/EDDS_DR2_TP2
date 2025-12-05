6e68cc099adefe4973a2532f0d92f3d1cfb49c55
Felipe Alves <89516641+5TheLoneWolf5@users.noreply.github.com>
2025-12-05 14:10:05 -0300

    Update Exercício_12.java

--- src/main/java/org/example/Exercício_12.java
+++ src/main/java/org/example/Exercício_12.java
@@ -1,5 +1,8 @@
 /*
  *
+ * O problema do default é que o Enum TipoNotificacao (usado no switch) já tem um número fixo de possibilidades.
+ * O importante é aplicar o switch de forma exaustiva para garantir que todas as possibilidades do tipo sejam cobertas.
+ * Colocar um default é redudante, e pode inclusive, causar problemas no futuro ao adicionar novos valores dentro do Enum; visto que correrão o risco de cair em default.  
+ * Este problema pode ser corrigido ao eliminar o default da estrutura.
  *
- *
- * */
+ */
