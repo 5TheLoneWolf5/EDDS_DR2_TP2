23ed03bdfafde5a3c46c6b9c9c51bc6847dc3f7e
Felipe Alves <89516641+5TheLoneWolf5@users.noreply.github.com>
2025-12-05 14:07:41 -0300

    Update Exercício_11.java

--- src/main/java/org/example/Exercício_11.java
+++ src/main/java/org/example/Exercício_11.java
@@ -1,5 +1,8 @@
 /*
  *
+ * O problema do default é que o Enum StatusPedido (usado no switch) já tem um número fixo de status.
+ * O importante é aplicar o switch de forma exaustiva para garantir que todas as possibilidades do tipo sejam cobertas.
+ * Colocar um default é redudante, e pode inclusive, causar problemas no futuro ao adicionar novos status dentro do Enum (visto que correrão o risco de cair em default).  
+ * Este problema pode ser corrigido ao eliminar o default da estrutura.
  *
- *
- * */
+ */
