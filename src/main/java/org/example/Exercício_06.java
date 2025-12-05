aed455742c78a8812cfeb8288318a3097d84e323
Felipe Alves <89516641+5TheLoneWolf5@users.noreply.github.com>
2025-12-04 22:48:29 -0300

    Update Exercício_06.java

--- src/main/java/org/example/Exercício_06.java
+++ src/main/java/org/example/Exercício_06.java
@@ -1,5 +1,5 @@
 /*
-*
-*
-*
+* O problema é que essa abordagem fere o princípio Aberto/Fechado do SOLID e cria alto acoplamento entre as nacionalidades e suas cores.
+* 
+* Ao invés de utilizar um switch statement, poderia-se utilizar um Enum ou melhor, aplicar o Strategy Pattern para resolver o problema junto de talvez um Factory para criação de objetos. 
 * */
