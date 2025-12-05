ae569b114e6ae42e215344ebc43ed79aefb12977
Felipe Alves <89516641+5TheLoneWolf5@users.noreply.github.com>
2025-12-05 14:10:57 -0300

    Update Exercício_07.java

--- src/main/java/org/example/Exercício_07.java
+++ src/main/java/org/example/Exercício_07.java
@@ -1,5 +1,5 @@
 /*
 * O problema é que esse código fere o princípio Aberto/Fechado do SOLID.
-* O uso excessivo de ifs aumenta a carga cognitiva do código. 
+* O uso excessivo de ifs aumenta a carga cognitiva e complexidade do código. 
 * Além disso, o invés de criar ou editar declarações if sempre que precisar adicionar ou alterar um tipo, poderia-se utilizar um Enum ou melhor, aplicar o Strategy Pattern para resolver o problema. 
 * */
